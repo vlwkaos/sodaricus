e674efd39321668ff3e4e197e190577901266b16
mgp <mgpark2274@hotmail.com>
2016-03-10 19:29:04 +0900

    from ini

--- core/src/com/lumibottle/game/GameMain.java
+++ core/src/com/lumibottle/game/GameMain.java
@@ -3,13 +3,15 @@ package com.lumibottle.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.lumibottle.helper.AssetLoader;
+import com.lumibottle.screen.GameScreen;
 
 
 public class GameMain extends Game {
 
 	@Override
 	public void create () {
-		Gdx.app.log("MyGdxGame", "created");
+		Gdx.app.log("GameMain", "created");
 		AssetLoader.load();
 		setScreen(new GameScreen());
 		//switching screen needs disposal too.
--- core/src/com/lumibottle/screen/GameScreen.java
+++ core/src/com/lumibottle/screen/GameScreen.java
@@ -1,10 +1,76 @@
 package com.lumibottle.screen;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.lumibottle.helper.InputHelper;
+import com.lumibottle.world.GameRenderer;
+import com.lumibottle.world.GameWorld;
+
 /**
  * Created by MG-POW on 2016-03-10.
  */
-public class GameScreen {
+public class GameScreen implements Screen{
+
+	private GameWorld myWorld;
+	private GameRenderer myRenderer;
+	private float runTime;
+
+	/*
+    *  set screen size, set mid point,
+    *  introduce GameWorld and GameRenderer, pass Gameworld to Renderer
+    *  does not interfere with any actual work.
+    *  call in inputhandler
+    * */
+
 	public GameScreen(){
-		System.out.printf("helloworld");
+		runTime = 0;
+		float screenWidth = Gdx.graphics.getWidth();
+		float screenHeight = Gdx.graphics.getHeight();
+		float gameWidth = 240;//fixed width
+		float gameHeight = screenHeight / (screenWidth / gameWidth);
+
+		int midPointY = (int) (gameHeight / 2);
+
+		myWorld = new GameWorld(midPointY);
+		myRenderer = new GameRenderer(myWorld, (int) gameHeight, midPointY);
+		Gdx.input.setInputProcessor(new InputHelper(myWorld));
+	}
+
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void render(float delta) {
+		runTime+=delta;
+		myWorld.update(delta);
+		myRenderer.render(runTime);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void dispose() {
+
 	}
 }
