caac5535b89c11421b3ab51408bb560fdbf68c50
mgcoder <mgpark2274@hotmail.com>
2016-03-16 09:19:16 +0900

    TODO : FX

---
+++ core/src/com/lumibottle/gameobjects/FX.java
@@ -0,0 +1,19 @@
+package com.lumibottle.gameobjects;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by MG-POW on 2016-03-15.
+ */
+public class FX {
+
+	private Vector2 position;
+
+	private String animationName;
+
+	private float runTime;
+	private float duration;
+
+
+
+}
--- core/src/com/lumibottle/gameobjects/ProgressHandler.java
+++ core/src/com/lumibottle/gameobjects/ProgressHandler.java
@@ -8,36 +8,43 @@ import com.badlogic.gdx.math.MathUtils;
  */
 public class ProgressHandler {
 
-    private Squirrel mySquirrel;
+	private Squirrel mySquirrel;
 
-    private int numOfEnemies;
+	private int numOfEnemies;
 
-    private float runTime;
+	private float runTime;
 
-    private RoadRoller[] roadRollers;
+	private RoadRoller[] roadRollers;
 
-    public ProgressHandler(Squirrel mySquirrel){
-        this.mySquirrel = mySquirrel;
-
-
-        roadRollers= new RoadRoller[3];
-        for (int i=0;i<roadRollers.length;i++){
-            roadRollers[i]= new RoadRoller();
-            roadRollers[i].reset(240+i*50);
-        }
-    }
-
-    public void update(float delta){
-        runTime+=delta;
-        for (RoadRoller r : roadRollers) {
-            r.update(delta);
-            r.collide(mySquirrel);
-        }
-
-    }
+	public ProgressHandler(Squirrel mySquirrel) {
+		this.mySquirrel = mySquirrel;
 
 
-    public RoadRoller[] getRoadRollers() {
-        return roadRollers;
-    }
+		roadRollers = new RoadRoller[3];
+		for (int i = 0; i < roadRollers.length; i++) {
+			roadRollers[i] = new RoadRoller();
+			roadRollers[i].reset(240 + i * 50);
+		}
+	}
+
+	public void update(float delta) {
+		runTime += delta;
+		for (RoadRoller r : roadRollers) {
+			r.update(delta);
+			r.collide(mySquirrel);//
+			/*
+			if (r.effectready)
+
+				fx.reset(animation name, position) ( available fx)
+			    */
+
+
+		}
+
+	}
+
+
+	public RoadRoller[] getRoadRollers() {
+		return roadRollers;
+	}
 }
