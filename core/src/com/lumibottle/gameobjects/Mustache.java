7b075ed67414d450b62669cfe4f203a02b8eb08d
mgcoder <mgpark2274@hotmail.com>
2016-03-21 09:23:23 +0900

    TODO: muscletache

--- core/src/com/lumibottle/gameobjects/Mustache.java
+++ core/src/com/lumibottle/gameobjects/Mustache.java
@@ -1,31 +1,39 @@
 package com.lumibottle.gameobjects;
 
-
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Vector2;
 import com.lumibottle.helper.FXHelper;
 
 /**
- * Created by MG-POW on 2016-03-13.
+ * Created by MG-POW on 2016-03-19.
  */
-public class RoadRoller extends GameEvent {
+public class Mustache extends GameEvent {
 
 	private Polygon hitbox;
 
-	public RoadRoller() {
+	private boolean doneMoving;
+
+
+
+	final private int speed=50;
+	private Vector2 acceleration;
+
+
+	public Mustache() {
 		super(20, 12);
 		hitbox = new Polygon(new float[]{0, 0, 20, 0, 20, 12, 0, 12});
 	}
 
+
+	//must separate moving, stopping
 	@Override
 	public void update(float delta) {
 		hitbox.setPosition(getX(), getY());
 
 		if (isVISIBLE()) {
 			getPosition().add(getVelocity().cpy().scl(delta));
-//		collide(mySquirrel); this is progress
 			if (isOutOfScreen(true))
 				ready();
 		}
@@ -33,8 +41,11 @@ public class RoadRoller extends GameEvent {
 	}
 
 	public void reset(float x) {
-		//	super.reset(x, MathUtils.random(GameEvent.gameHeight), -50, 0, 0);
-		super.reset(x, MathUtils.random(90, 110), -50, 0, 0);
+		super.reset(x, MathUtils.random(90, 110), 0, 0, 0);
+	}
+
+	public void setDoneMoving(boolean yes){
+		doneMoving=yes;
 	}
 
 
@@ -58,7 +69,4 @@ public class RoadRoller extends GameEvent {
 			}
 		}
 	}
-
 }
-
-
