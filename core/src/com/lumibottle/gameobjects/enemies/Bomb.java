dd9045a017df8eebd980302c8e82fdaca4da255b
mgcoder <mgpark2274@hotmail.com>
2016-03-30 09:23:27 +0900

    added bomb class

--- core/src/com/lumibottle/gameobjects/enemies/Bomb.java
+++ core/src/com/lumibottle/gameobjects/enemies/Bomb.java
@@ -3,5 +3,14 @@ package com.lumibottle.gameobjects.enemies;
 /**
  * Created by MG-POW on 2016-03-27.
  */
-public class Bomb {
+public class Bomb extends RoadRoller{
+
+	@Override
+	public void update(float delta){
+
+	}
+
+
+
+
 }
